1412b646c3b0752a5bc868a5b1f22073d3704f53
Tiago Rosa da costa <tiagorosadacost@gmail.com>
2020-03-19 23:04:00 -0300

    Playing vue.js

---
+++ src/components/Category.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="slider">
+      
+      <h3>{{title}}</h3>
+      <span v-on:mouseover="scrollToLeft()" v-on:mouseout="clearScroll()" class="handle handlePrev active">
+        <i class="fa fa-caret-left" aria-hidden="true"></i>
+      </span>
+
+      <div ref="scroller" class="row">
+        <div class="row__inner">
+          <Movie 
+            title="teste" 
+            source="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700"></Movie>
+         
+          <Movie 
+            title="teste" 
+            source="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700"></Movie>
+         
+          <Movie 
+            title="teste" 
+            source="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700"></Movie>
+         
+          <Movie 
+            title="teste" 
+            source="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700"></Movie>
+         
+          <Movie 
+            title="teste" 
+            source="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700"></Movie>
+         
+          <Movie 
+            title="teste" 
+            source="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700"></Movie>
+         
+          <Movie 
+            title="teste" 
+            source="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700"></Movie>
+         
+          <Movie 
+            title="teste" 
+            source="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700"></Movie>
+         
+         
+        </div>
+      </div>
+      <span v-on:mouseover="scrollToRight()" v-on:mouseout="clearScroll()"
+       class="handle handleNext active">
+        <i class="fa fa-caret-right" aria-hidden="true"></i>
+      </span>
+    </div>
+</template>
+
+<script>
+
+import Movie from "./Movie";
+
+export default {
+    name: "Category",
+    props: [ "title" ],
+    components: {
+        Movie: Movie
+    },
+    datas: {
+        intervalo: null
+    },
+    methods: {
+        scrollToRight: function() {
+        this.intervalo = setInterval(() => this.$refs.scroller.scrollLeft += 1, 5);
+        },
+
+        scrollToLeft: function() {
+        this.intervalo = setInterval(() => this.$refs.scroller.scrollLeft -= 1, 5);
+        },
+
+        clearScroll: function() {
+            clearInterval(this.intervalo);
+        }
+    },
+}
+</script>
---
+++ src/components/Movie.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="gui-card" 
+        v-on:mouseout="removeFocus"
+        v-on:mouseover="applyFocus" v-bind:style="focus">
+        <div class="gui-card__media">
+            <img class="gui-card__img" v-bind:src="source" alt="" />
+        </div>
+        <div class="gui-card__details">
+        <div class="gui-card__title">
+            {{ title }}
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Movie",
+    props: [ "title", "source" ],
+    data() {
+        return {
+            focus: false
+        }
+    },
+    methods: {
+        applyFocus() {
+             this.focus = `
+                color: red;
+                transform: scale(1.9);
+                margin-right: 110px;
+            `;
+        },
+        removeFocus() {
+            this.focus = "";
+        }
+    }
+}
+</script>
+
+<style>
+</style>
